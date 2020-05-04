b95c5a5faeb01dee7cdf65f47e334a94a78d439c
john <you@example.com>
2020-05-04 13:28:47 +1200

    authorizationFilter working

--- src/main/java/com/appdeveloperblog/app/ws/Security/WebSecurity.java
+++ src/main/java/com/appdeveloperblog/app/ws/Security/WebSecurity.java
@@ -29,9 +29,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().
-		disable().authorizeRequests().antMatchers(HttpMethod.POST,SecurityConstants.SIGN_UP_URL).permitAll()
+		disable().authorizeRequests().antMatchers(HttpMethod.POST,SecurityConstants.SIGN_UP_URL)
+		.permitAll()
 		.anyRequest().authenticated()
-		.and().addFilter(getAuthenticationFilter());
+		.and()
+		.addFilter(getAuthenticationFilter())
+		.addFilter(new AuthorizationFilter(authenticationManager()));
 	}
 	
 //	it's for signin entering plain password to encrypt
